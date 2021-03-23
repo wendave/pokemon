b0323f508f2a84896befd5696f5ef0ca0734cfd8
owen <owendavecaisip@gmail.com>
2021-03-23 16:53:19 +0800

    Home Page - Section Gameboy - In Progress

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -34,137 +34,148 @@ var headerFunctions = () => {
   headerLoaded();
 }
 
-// home page - hero section
-var homeHero = () => {  
-  var sectionPokeball = document.getElementsByClassName("wrapper-pokeballs");
-  var heroBackground = document.getElementById('hero_background');
-  var pokeball = document.getElementById('wrapper_pokeballs').getElementsByClassName('pokeball');
-  var pokeslides = document.querySelector(".swiper-starter").getElementsByClassName('swiper-slide');
+// home page - section hero
+var homeHero = () => {
+  const sectionHero = document.querySelectorAll("#wrapper_pokeballs");
 
-  function hoverPokeball() {
-    for (var i = 0; i < pokeball.length; i++) {
-      pokeball[i].addEventListener("mouseover", function() {
-        var imageSrc = this.dataset.image;
-        heroBackground.style.backgroundImage = 'url("'+imageSrc+'")';
-        // console.log(this);
-      }),
-      pokeball[i].addEventListener("mouseleave", function() {
-        var defaultBlack = "../assets/images/bg-black.jpg"; 
-        heroBackground.style.backgroundImage = 'url("'+defaultBlack+'")';
-      });
-    }
-  }
+  if (sectionHero.length) {    
+    var sectionPokeball = document.getElementsByClassName("wrapper-pokeballs");
+    var heroBackground = document.getElementById('hero_background');
+    var pokeball = document.getElementById('wrapper_pokeballs').getElementsByClassName('pokeball');
+    var pokeslides = document.querySelector(".swiper-starter").getElementsByClassName('swiper-slide');
 
-  function clickPokeball() {
-    // $(".swiper-pagination-clickable .swiper-pagination-bullet:nth-child(3)").click();
-
-    // section swiper - add data-slide for each slide
-    for (var i = 0; i < pokeslides.length; i++) {
-      var pokeSlide = i + 1;
-      pokeslides[i].dataset.pokemonSlide = pokeSlide;
+    function hoverPokeball() {
+      for (var i = 0; i < pokeball.length; i++) {
+        pokeball[i].addEventListener("mouseover", function() {
+          var imageSrc = this.dataset.image;
+          heroBackground.style.backgroundImage = 'url("'+imageSrc+'")';
+          // console.log(this);
+        }),
+        pokeball[i].addEventListener("mouseleave", function() {
+          var defaultBlack = "../assets/images/bg-black.jpg"; 
+          heroBackground.style.backgroundImage = 'url("'+defaultBlack+'")';
+        });
+      }
     }
 
-    // section hero - add data-number for each item
-    for (var i = 0; i < pokeball.length; i++) {
-      var pokeNumber = i + 1;
-      pokeball[i].dataset.pokemonNumber = pokeNumber;
+    function clickPokeball() {
+      // $(".swiper-pagination-clickable .swiper-pagination-bullet:nth-child(3)").click();
 
-      pokeball[i].addEventListener("click", function() {
-        var selectedNumber = Number(this.dataset.pokemonNumber) + 1;
-        
-        // scroll to next section
-        // setTimeout(() => {
-        //   document.querySelector('.swiper-starter').scrollIntoView({
-        //     behavior: 'smooth',
-        //     duration: 2500
-        //   });
-        // }, 500);
-        // document.querySelector('.swiper-starter').scrollIntoView({
-        //   behavior: 'smooth'
-        // });
-        $("html, body").animate({
-          scrollTop: $(".swiper-starter").offset().top
-        }, 1500);
+      // section swiper - add data-slide for each slide
+      for (var i = 0; i < pokeslides.length; i++) {
+        var pokeSlide = i + 1;
+        pokeslides[i].dataset.pokemonSlide = pokeSlide;
+      }
 
-        if ( selectedNumber == 2 ) {
-          document.querySelector(".swiper-starter .swiper-pagination-bullet:nth-child(2)").click();
-        }
-        else if ( selectedNumber == 3 ) {
-          document.querySelector(".swiper-starter .swiper-pagination-bullet:nth-child(3)").click();
-        }
-        else if ( selectedNumber == 4 ) {
-          document.querySelector(".swiper-starter .swiper-pagination-bullet:nth-child(4)").click();
-        }
-        else if ( selectedNumber == 5 ) {
-          document.querySelector(".swiper-starter .swiper-pagination-bullet:nth-child(5)").click();
-        }
-      });
+      // section hero - add data-number for each item
+      for (var i = 0; i < pokeball.length; i++) {
+        var pokeNumber = i + 1;
+        pokeball[i].dataset.pokemonNumber = pokeNumber;
+
+        pokeball[i].addEventListener("click", function() {
+          var selectedNumber = Number(this.dataset.pokemonNumber) + 1;
+          
+          // scroll to next section
+          // setTimeout(() => {
+          //   document.querySelector('.swiper-starter').scrollIntoView({
+          //     behavior: 'smooth',
+          //     duration: 2500
+          //   });
+          // }, 500);
+          // document.querySelector('.swiper-starter').scrollIntoView({
+          //   behavior: 'smooth'
+          // });
+          $("html, body").animate({
+            scrollTop: $(".swiper-starter").offset().top
+          }, 1500);
+
+          if ( selectedNumber == 2 ) {
+            document.querySelector(".swiper-starter .swiper-pagination-bullet:nth-child(2)").click();
+          }
+          else if ( selectedNumber == 3 ) {
+            document.querySelector(".swiper-starter .swiper-pagination-bullet:nth-child(3)").click();
+          }
+          else if ( selectedNumber == 4 ) {
+            document.querySelector(".swiper-starter .swiper-pagination-bullet:nth-child(4)").click();
+          }
+          else if ( selectedNumber == 5 ) {
+            document.querySelector(".swiper-starter .swiper-pagination-bullet:nth-child(5)").click();
+          }
+        });
+      }
     }
-  }
 
-  // test time
-  setTimeout(() => {
-    document.getElementById("home_hero_pokeball_image").classList.remove("zoom");
-  }, 100);
-  setTimeout(() => {
-    document.getElementById("home_hero_pokeball_image").classList.add("opacity-0");
-  }, 300);
-  setTimeout(() => {
-    document.getElementById("typing_text").parentElement.classList.remove("opacity-0");
-    document.getElementById("typing_text").parentElement.classList.add("opacity-100");
-  }, 700);
-  setTimeout(() => {
-    document.getElementById("typing_text").parentElement.classList.remove("opacity-100");
-    document.getElementById("typing_text").parentElement.classList.add("opacity-0");
-  }, 800);
-  
-  if (sectionPokeball.length) {
+    // test time
     setTimeout(() => {
-      document.getElementById("wrapper_pokeballs").classList.add("show-element");
-    }, 1000);
+      document.getElementById("home_hero_pokeball_image").classList.remove("zoom");
+    }, 100);
+    setTimeout(() => {
+      document.getElementById("home_hero_pokeball_image").classList.add("opacity-0");
+    }, 300);
+    setTimeout(() => {
+      document.getElementById("typing_text").parentElement.classList.remove("opacity-0");
+      document.getElementById("typing_text").parentElement.classList.add("opacity-100");
+    }, 700);
+    setTimeout(() => {
+      document.getElementById("typing_text").parentElement.classList.remove("opacity-100");
+      document.getElementById("typing_text").parentElement.classList.add("opacity-0");
+    }, 800);
+    
+    if (sectionPokeball.length) {
+      setTimeout(() => {
+        document.getElementById("wrapper_pokeballs").classList.add("show-element");
+      }, 1000);
 
-    hoverPokeball();
-    clickPokeball();
+      hoverPokeball();
+      clickPokeball();
+    }
+
+    // real time
+    // setTimeout(() => {
+    //   document.getElementById("home_hero_pokeball_image").classList.remove("zoom");
+    // }, 1300);
+    // setTimeout(() => {
+    //   document.getElementById("home_hero_pokeball_image").classList.add("opacity-0");
+    // }, 3500);
+    // setTimeout(() => {
+    //   document.getElementById("typing_text").parentElement.classList.remove("opacity-0");
+    //   document.getElementById("typing_text").parentElement.classList.add("opacity-100");
+    // }, 4000);
+    // setTimeout(() => {
+    //   document.getElementById("typing_text").parentElement.classList.remove("opacity-100");
+    //   document.getElementById("typing_text").parentElement.classList.add("opacity-0");
+    // }, 9000);
+    
+    // if (sectionPokeball.length) {
+    //   setTimeout(() => {
+    //     document.getElementById("wrapper_pokeballs").classList.add("show-element");
+    //   }, 9200);
+
+    //   hoverPokeball();
+    //   clickPokeball();
+    // }
   }
-
-  // real time
-  // setTimeout(() => {
-  //   document.getElementById("home_hero_pokeball_image").classList.remove("zoom");
-  // }, 1300);
-  // setTimeout(() => {
-  //   document.getElementById("home_hero_pokeball_image").classList.add("opacity-0");
-  // }, 3500);
-  // setTimeout(() => {
-  //   document.getElementById("typing_text").parentElement.classList.remove("opacity-0");
-  //   document.getElementById("typing_text").parentElement.classList.add("opacity-100");
-  // }, 4000);
-  // setTimeout(() => {
-  //   document.getElementById("typing_text").parentElement.classList.remove("opacity-100");
-  //   document.getElementById("typing_text").parentElement.classList.add("opacity-0");
-  // }, 9000);
-  
-  // if (sectionPokeball.length) {
-  //   setTimeout(() => {
-  //     document.getElementById("wrapper_pokeballs").classList.add("show-element");
-  //   }, 9200);
-
-  //   hoverPokeball();
-  //   clickPokeball();
-  // }
 }
 
 // swipers
 var swipers = () => {
-  var pokemonStarter = new Swiper('.swiper-starter .swiper-container', {
-    touchRatio: 0,
-    pagination: {
-      el: '.swiper-starter .swiper-pagination',
-      clickable: true,
-      // renderBullet: function (index, className) {
-      //   return '<span class="' + className + '">' + (index + 1) + '</span>';
-      // },
-    },
-  });
+  const sectionSwiperStarter = document.querySelectorAll(".swiper-starter");
+
+  if (sectionSwiperStarter.length) {
+    var pokemonStarter = new Swiper('.swiper-starter .swiper-container', {
+      touchRatio: 0,
+      pagination: {
+        el: '.swiper-starter .swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+}
+
+// home page - section gameboy
+var gameboy = () => {
+  const sectionGameboy = document.querySelectorAll(".gameboy");
+
 }
 
 
@@ -173,3 +184,4 @@ preloader();
 headerFunctions();
 homeHero();
 swipers();
+gameboy();
